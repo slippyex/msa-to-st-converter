369617412d77ecb749ad585813c290fec4808c42
slippyvex <markus.velten@gmx.de>
2024-05-16 10:31:41 +0200

    don't fail on corrupt msa images anymore

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,17 @@ struct MsaHeader {
     end_track: u16,
 }
 
-fn read_msa_header(msa_data: &[u8]) -> io::Result<MsaHeader> {
+fn read_msa_header(msa_data: &[u8]) -> Option<MsaHeader> {
     if msa_data.len() < 10 {
-        return Err(io::Error::new(io::ErrorKind::InvalidInput, "MSA data is too short."));
+        return None;
     }
 
     let id_marker = u16::from_be_bytes([msa_data[0], msa_data[1]]);
     if id_marker != 0x0E0F {
-        return Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid MSA file. ID Marker does not match."));
+        return None;
     }
 
-    Ok(MsaHeader {
+    Some(MsaHeader {
         sectors_per_track: u16::from_be_bytes([msa_data[2], msa_data[3]]),
         sides: u16::from_be_bytes([msa_data[4], msa_data[5]]) + 1,
         start_track: u16::from_be_bytes([msa_data[6], msa_data[7]]),
@@ -54,8 +54,11 @@ fn process_data_track(track_data_length: usize, msa_data: &[u8], msa_index: usiz
     Ok((st_data, index))
 }
 
-fn decode_msa_to_st(msa_data: &[u8]) -> io::Result<Vec<u8>> {
-    let msa_header = read_msa_header(msa_data)?;
+fn decode_msa_to_st(msa_data: &[u8]) -> Option<Vec<u8>> {
+    let msa_header = match read_msa_header(msa_data) {
+        None => { println!("Invalid MSA header data"); return None; },
+        Some(header) => header,
+    };
 
     let mut msa_index = 10;
     let mut st_data = Vec::new();
@@ -63,12 +66,12 @@ fn decode_msa_to_st(msa_data: &[u8]) -> io::Result<Vec<u8>> {
         for _side in 0..msa_header.sides {
             let track_data_length = u16::from_be_bytes([msa_data[msa_index], msa_data[msa_index + 1]]) as usize;
             msa_index += 2;
-            let (mut track_data, new_index) = process_data_track(track_data_length, msa_data, msa_index, msa_header.sectors_per_track)?;
+            let (mut track_data, new_index) = process_data_track(track_data_length, msa_data, msa_index, msa_header.sectors_per_track).ok()?;
             st_data.append(&mut track_data);
             msa_index = new_index;
         }
     }
-    Ok(st_data)
+    Some(st_data)
 }
 
 fn traverse_directory(src_dir: &Path, dest_dir: &Path) -> io::Result<()> {
@@ -86,7 +89,16 @@ fn traverse_directory(src_dir: &Path, dest_dir: &Path) -> io::Result<()> {
                 let st_path = PathBuf::from(dest_dir).join(format!("{}.st", original_basename));
                 let mut msa_data = Vec::new();
                 File::open(&src_path)?.read_to_end(&mut msa_data)?;
-                let st_data = decode_msa_to_st(&msa_data)?;
+                let st_data = match decode_msa_to_st(&msa_data) {
+                    None => {
+                        println!(
+                            "Could not convert {}. Skipping this file.",
+                            src_path.display()
+                        );
+                        continue;
+                    },
+                    Some(data) => data
+                };
                 File::create(st_path)?.write_all(&st_data)?;
                 println!("Converted {} successfully.", src_path.display());
             }
